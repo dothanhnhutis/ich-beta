1d7880d2d73d0276375fba56e9640bf0c5d199ad
dothanhnhutis <dothanhnhutis@gmail.com>
2024-12-31 01:09:32 +0700

    update....

---
+++ client/app/(tv)/tv1/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Metadata } from "next";
+import Display1 from "./display1";
+export const metadata: Metadata = {
+  title: "Phòng ban",
+};
+const Task1Page = () => {
+  return <Display1 />;
+};
+
+export default Task1Page;
